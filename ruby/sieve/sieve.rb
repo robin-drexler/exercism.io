1dfd8fda417fd8e4995bc46ea0ac173d5cbc5b4b
Robin Drexler <drexler.robin@gmail.com>
2015-03-16 21:29:00 +0100

    shorter implementation for sieve

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -5,25 +5,19 @@ class Sieve
 
   def primes
     numbers = (2..@to).to_a
-    to = @to
-    current_number = numbers.first()
+    primes = []
 
-    loop do
+    while numbers.length > 0 do
+      current_number = numbers.first()
+      primes << current_number
+
       # only keep numbers that aren't multiples of current number
-      # shrinking the array with every iteration and speeding up for large ones
-      numbers.select!() do |number|
-        number == current_number || number % current_number != 0
+      # this also removes current_number itself
+      numbers.reject!() do |number|
+        number % current_number === 0
       end
-
-      # find next number, that is greater than current
-      # if there is none left, break
-      current_number = numbers.find() do |number|
-        number > current_number
-      end
-
-      break if current_number.nil?
     end
 
-    numbers
+    primes
   end
 end
