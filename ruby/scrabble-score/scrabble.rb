94d3c926a97fc4158523fc37753f1b6f4ad12fc9
Robin Drexler <drexler.robin@gmail.com>
2015-03-27 16:54:16 +0100

    make rubocop more happy

--- ruby/scrabble-score/scrabble.rb
+++ ruby/scrabble-score/scrabble.rb
@@ -1,12 +1,12 @@
 class Scrabble
   SCORES = {
-    1 => %w{a e i o u l n r s t},
-    2 => %w{d g},
-    3 => %w{b c m p},
-    4 => %w{f h v w y},
-    5 => %w{k},
-    8 => %w{j x},
-    10 => %w{q z},
+    1 => %w(a e i o u l n r s t),
+    2 => %w(d g),
+    3 => %w(b c m p),
+    4 => %w(f h v w y),
+    5 => %w(k),
+    8 => %w(j x),
+    10 => %w(q z)
   }
 
   def initialize(word)
@@ -24,7 +24,7 @@ class Scrabble
 
   def score
     @word.chars.reduce(0) do |score, char|
-      score += @scores_lookup[char]
+      score + @scores_lookup[char]
     end
   end
 
